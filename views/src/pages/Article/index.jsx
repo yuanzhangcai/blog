e49d6cad7d35cf281cba101f4d9accf5bd583bfd
zacyuan <yuanchangcai@papegames.net>
2021-03-18 20:43:50 +0800

    调整文章详细页title

--- views/src/pages/Article/index.jsx
+++ views/src/pages/Article/index.jsx
@@ -1,9 +1,10 @@
-import { Divider, Col, Row } from 'antd';
+import { Divider, Col, Row, PageHeader } from 'antd';
 import React, { Component } from 'react';
 import { connect } from 'umi';
 import styles from './Article.less';
 import CommentEditor from './CommentEditor';
 import { LikeOutlined, MessageFilled, EyeOutlined } from '@ant-design/icons';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const IconText = ({ icon, text }) => (
   <span className={styles.iconText}>
@@ -13,21 +14,28 @@ const IconText = ({ icon, text }) => (
 
 class Article extends Component {
   componentDidMount() {
-    const { dispatch } = this.props; // dispatch({
+    const { dispatch } = this.props;
+    // dispatch({
     //   type: 'article/fetch',
     // });
   }
 
   render() {
     return (
-      <section>
-        <h1>开端</h1>
-        <div className={styles.modifiedTime}>2020-10-09 13:54</div>
-        <Divider
-          style={{
-            marginBottom: 20,
-          }}
-        />
+      <HelmetProvider>
+        <Helmet>
+          <title>开端</title>
+          <meta name="description" content={"开端"} />
+        </Helmet>
+        <PageHeader
+          ghost={false}
+          onBack={() => window.history.back()}
+          title="开端"
+          extra={[
+            <div className={styles.modifiedTime}>2020-10-09 13:54</div>
+          ]}
+        >
+        </PageHeader>
         <p>个人博客的开启。（系统正在一步一步建设中。。。）</p>
         <Divider
           style={{
@@ -42,7 +50,7 @@ class Article extends Component {
           </Col>
         </Row>
         <CommentEditor />
-      </section>
+      </HelmetProvider>
     );
   }
 }
